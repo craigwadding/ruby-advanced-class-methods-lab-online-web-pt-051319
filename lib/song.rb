27a6efaf3048788a4e3c4b41cbbc32ada6c35f9f
craig wadding <craig.wadding@gmail.com>
2019-06-09 03:07:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,36 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new 
+    song 
+  end 
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name 
+    song 
+  end 
+  
+  def self.creat_by_name(name)
+    song = self.new 
+    song.name = name 
+    @@all << song 
+    song 
+  end 
+  
+  def self.find_by_name(name)
+    @@all.find{|x| x.name == name}
+  end 
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.find_or_create_by_name(name)
+  end 
+  
+  def self.alphabetical() 
+    @@all.sort_by{|x| x.name} 
+  end 
+  
+  
 end
